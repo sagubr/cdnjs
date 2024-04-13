7fae6d37df40fa4c590a1a90fb42ac448bce44a9
Gustavo Garcia <sousagustavogarcia@gmail.com>
2024-04-13 14:54:09 -0300

    refac: simplified dropdown

--- dropdown.js
+++ dropdown.js
@@ -1,13 +1,9 @@
-function touchDropdown() {
+function toggleDropdown() {
 	console.log("sagubr/cdnjs/dropdown.js");
-	document.getElementById("_dropdown").classList.toggle("show");
-}
-
-window.onclick = function (e) {
-	if (!e.target.matches(".dropbtn")) {
-		let dropdown = document.getElementById("_dropdown");
-		if (dropdown.classList.contains("show")) {
-			dropdown.classList.remove("show");
-		}
+	let dropdown = document.getElementById("_dropdown");
+	if (dropdown.style.display === "none") {
+		dropdown.style.display = "block";
+	} else {
+		dropdown.style.display = "none";
 	}
-};
+}
