2f183c5dbfdc92b87884088ad2d5d7284f45ced6
Gustavo Garcia <sousagustavogarcia@gmail.com>
2024-04-13 14:17:17 -0300

    first commit

---
+++ dropdown.js
@@ -0,0 +1,12 @@
+function touchDropdown() {
+	document.getElementById("_dropdown").classList.toggle("show");
+}
+
+window.onclick = function (e) {
+	if (!e.target.matches(".dropbtn")) {
+		let dropdown = document.getElementById("_dropdown");
+		if (dropdown.classList.contains("show")) {
+			dropdown.classList.remove("show");
+		}
+	}
+};
